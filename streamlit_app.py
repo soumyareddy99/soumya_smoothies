5a039dd595fa166c44d4bcb7239321426d6ddc74
soumyareddy99 <vsoumya197@gmail.com>
2026-03-13 16:21:40 -0500

    Update streamlit_app.py
    Fetch watermelon data from Smoothiefroot API and display it.

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,4 +27,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/Watermelon")
+st.text(smoothiefroot_response
                     
