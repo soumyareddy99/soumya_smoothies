cefbedcb5f4c20b5fbf08ef25cc06b85aac2a1cb
soumyareddy99 <vsoumya197@gmail.com>
2026-03-13 16:35:26 -0500

    Update streamlit_app.py
    Fix syntax error in success message display

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,5 +29,5 @@ if ingredients_list:
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")True)
+        st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
                     
